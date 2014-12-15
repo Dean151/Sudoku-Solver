d3b04d3c213e1847b3d417d9bb99eebbcbe88d27
Dean151 <thomas.durand.verjat@gmail.com>
2014-12-15 15:20:33 +0100

    Project created with Xcode 6.2 Beta (6C95a)

---
+++ Sudoku-Solver/Sudoku-SolverTests/Sudoku_SolverTests.swift
@@ -0,0 +1,36 @@
+//
+//  Sudoku_SolverTests.swift
+//  Sudoku-SolverTests
+//
+//  Created by Thomas Durand on 15/12/2014.
+//  Copyright (c) 2014 Dean151. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Sudoku_SolverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
