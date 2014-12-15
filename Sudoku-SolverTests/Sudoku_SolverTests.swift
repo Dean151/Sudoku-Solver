23e224359d44a93921e727eeb86d7f9cd3046543
Dean151 <thomas.durand.verjat@gmail.com>
2014-12-15 18:00:15 +0100

    test file i cannot manage to make work for now

--- Sudoku-SolverTests/Sudoku_SolverTests.swift
+++ Sudoku-SolverTests/Sudoku_SolverTests.swift
@@ -9,6 +9,8 @@
 import UIKit
 import XCTest
 
+import Sudoku_Solver
+
 class Sudoku_SolverTests: XCTestCase {
     
     override func setUp() {
@@ -20,7 +22,7 @@ class Sudoku_SolverTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+    /*
     func TestPeers() {
         // FIXME import Sudoku.swift somehow...
         var p = Sudoku(length: 9);
@@ -30,6 +32,33 @@ class Sudoku_SolverTests: XCTestCase {
         XCTAssert(peers == expectedResult, "OK for Peers")
     }
     
+    func TestSolver() {
+        var p = Sudoku(values: [
+            0, 0, 3, 0, 2, 0, 6, 0, 0,
+            9, 0, 0, 3, 0, 5, 0, 0, 1,
+            0, 0, 1, 8, 0, 6, 4, 0, 0,
+            0, 0, 8, 1, 0, 2, 9, 0, 0,
+            7, 0, 0, 0, 0, 0, 0, 0, 8,
+            0, 0, 6, 7, 0, 8, 2, 0, 0,
+            0, 0, 2, 6, 0, 9, 5, 0, 0,
+            8, 0, 0, 2, 0, 3, 0, 0, 9,
+            0, 0, 5, 0, 1, 0, 3, 0, 0
+            ])
+        var solution = [
+            4, 8, 3, 9, 2, 1, 6, 5, 7,
+            9, 6, 7, 3, 4, 5, 8, 2, 1,
+            2, 5, 1, 8, 7, 6, 4, 9, 3,
+            5, 4, 8, 1, 3, 2, 9, 7, 6,
+            7, 2, 9, 5, 6, 4, 1, 3, 8,
+            1, 3, 6, 7, 9, 8, 2, 4, 5,
+            3, 7, 2, 6, 8, 9, 5, 1, 4,
+            8, 1, 4, 2, 5, 3, 7, 6, 9,
+            6, 9, 5, 4, 1, 7, 3, 8, 2
+        ]
+        
+        let solved = p.solve(sudoku: p)
+    }
+    */
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock() {
