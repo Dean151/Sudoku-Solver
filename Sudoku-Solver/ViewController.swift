00ad5688208e681d45046e6dd2885eeb1bb8fc16
Dean151 <thomas.durand.verjat@gmail.com>
2014-12-16 11:31:11 +0100

    Grid added, all labels added recursively to UI return

--- Sudoku-Solver/ViewController.swift
+++ Sudoku-Solver/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var labels: [UILabel]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Setting up labels
+        labels = [UILabel]()
+        for i in 0..<81 {
+            let l = UILabel(frame: CGRect(x: 0, y: 0, width: 70, height: 70))
+            l.text = ".";
+            l.textAlignment = NSTextAlignment.Center
+            l.textColor = UIColor.blackColor()
+            
+            let x = (i%9)*35 + 19
+            let y = Int(floor(Double(i)/9)*35) + 145
+            
+            l.center = CGPoint(x: x, y: y)
+            
+            labels.append(l)
+            self.view.addSubview(l)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +41,3 @@ class ViewController: UIViewController {
 
 
 }
-
