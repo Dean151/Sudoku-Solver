38fc987d1d608a42386b69547ea52441945c407e
Dean151 <thomas.durand.verjat@gmail.com>
2014-12-16 12:15:37 +0100

    Changing numbers on tap, clearing all numbers by UITabViewItem

--- Sudoku-Solver/ViewController.swift
+++ Sudoku-Solver/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var grid: UIImageView!
     var labels: [UILabel]!
 
     override func viewDidLoad() {
@@ -33,11 +34,50 @@ class ViewController: UIViewController {
             self.view.addSubview(l)
         }
     }
+    
+    // Should return a int between 0 and 80 if we click inside a cell, nil otherwise
+    func getCell(point: CGPoint) -> Int? {
+        if point.y > 120 && point.y < 440 {
+            let cell = Int( floor((point.y-120)/35)*9 + floor((point.x-1)/35) )
+            return cell
+        } else {
+            return nil
+        }
+    }
+    
+    // Change the number of a cell on a tap
+    func changeNumber(index: Int) {
+        let label = labels[index] as UILabel
+        if let value = label.text {
+            if value == "9" {
+                label.text = "."
+            } else if value == "." {
+                label.text = "1"
+            } else {
+                label.text = "\(value.toInt()! + 1)"
+            }
+        }
+    }
+    
+    // Clear all numbers
+    @IBAction func clearAll(sender: AnyObject) {
+        for label in labels {
+            label.text = "."
+        }
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        if let touch: AnyObject = touches.anyObject() {
+            let p = touch.locationInView(nil)
+            if let index = getCell(p) {
+                changeNumber(index)
+            }
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
